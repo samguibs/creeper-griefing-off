97ffdd54f63de009a771bf4ed0b13b3ecf9547c6
samguibs <samuelguibs@gmail.com>
2026-09-23 23:56:04 -0300

    Release - Fabric 26.2

---
+++ src/main/java/com/samguibs/creepergriefingoff/CreeperGriefingOff.java
@@ -0,0 +1,12 @@
+package com.samguibs.creepergriefingoff;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CreeperGriefingOff implements ModInitializer {
+
+    public static final String MOD_ID = "creeper-griefing-off";
+
+    @Override
+    public void onInitialize() {
+    }
+}
